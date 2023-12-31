66dbc98211a885445aad77cd31550f43e9d48ec0
AbhishekAle <aviale017@gmail.com>
2023-12-31 13:40:23 +0545

    userModel created

---
+++ server/models/userModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timeStamps: true }
+);
+
+const UserModel = mongoose.model("User", userSchema);
+
+export default UserModel;
